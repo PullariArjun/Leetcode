cfa5a36c0913ce32f59614cd8ce75cff13767bc3
PullariArjun <98091330+PullariArjun@users.noreply.github.com>
2024-02-08 21:13:09 +0530

    Time: 40 ms (51.89%) | Memory: 44.6 MB (19.70%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numSquares(int n) {
+        if(n < 4)return n;
+        if(n == 4)return 1;
+        int dp[] = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+        dp[4] = 1;
+        for(int i = 1; i*i <= n; i++){
+            dp[i*i] = 1;
+        }
+        for(int i = 5; i <= n; i++){
+            if(dp[i] == 1)continue;
+            // int x = (int)Math.sqrt(i);
+            int min = Integer.MAX_VALUE;
+            for(int j = 1; j*j <= i; j++){
+                min = Math.min(min,  dp[i - j * j ]);
+            }
+            dp[i] = min+1;
+        }
+        System.out.println(Arrays.toString(dp));
+        return dp[n];
+        //8 14 15 19 
+    }
+}
